6ee62255da55d500b3f165d34103fdab86495a3d
Shyam sundar R <shyamsundarr10@gmail.com>
2023-07-12 14:55:14 +0530

    time stamp with separated files

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ const PORT = 8200;
 // create server endpoint
 app.get("/current/date-time",(req,res)=>{
 
-    // Get a newdate
-    // let date = new Date();
-    // let content = date.toString();
+    //Get a newdate
+    let date = new Date();
+    let content = date.toString();
 
-    // // write a newfile "current time-date"
-    // fs.writeFile("./currentTime-date.txt",content,(err)=>{
-    //     if(err){
-    //         res.send("Error occured")
-    //     }else{
+    // write a newfile "current time-date"
+    fs.writeFile("./currentTime-date.txt",content,(err)=>{
+        if(err){
+            res.send("Error occured")
+        }else{
 
-    //         // after writing read a existing file
-    //         fs.readFile("./currentTime-date.txt","Utf-8",(err,data)=>{
-    //             if(err){
-    //                 res.send(`Error occured : ${err}`)
-    //             }else{
-    //                 res.send(data)
-    //             }
-    //         })
-    //     }
-    // })
-    res.send("Am working propery right")
+            // after writing read a existing file
+            fs.readFile("./currentTime-date.txt","Utf-8",(err,data)=>{
+                if(err){
+                    res.send(`Error occured : ${err}`)
+                }else{
+                    res.send(data)
+                }
+            })
+        }
+    })
+    
 })
 
 app.listen(PORT,()=>console.log(`the server is online : ${PORT}`))
